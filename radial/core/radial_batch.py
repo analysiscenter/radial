85fd224714273ca03a793283b9ea2e964102bd03
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2018-10-12 22:21:43 +0300

    Add initial RadialBatch

---
+++ radial/core/radial_batch.py
@@ -0,0 +1,160 @@
+""" Batch class for radial regime regression. """
+
+import numpy as np
+
+from .. import dataset as ds
+
+class RadialBatch(ds.Batch):
+    """
+    Batch class that stores multiple time series along with other parameters
+    for radial flow regime regression.
+
+    Parameters
+    ----------
+    index : DatasetIndex
+        Unique identifiers of ECGs in the batch.
+    preloaded : tuple, optional
+        Data to put in the batch if given. Defaults to ``None``.
+
+    Attributes
+    ----------
+    index : DatasetIndex
+        Unique identifiers of ECGs in the batch.
+    """
+
+    def __init__(self, index, preloaded=None):
+        super().__init__(index, preloaded)
+        self.time = self.array_of_nones
+        self.pressure = self.array_of_nones
+        self.derivative = self.array_of_nones
+        self.rig_type = self.array_of_nones
+        self.target = self.array_of_nones
+
+    @property
+    def components(self):
+        """tuple of str: Data components names."""
+        return "time", "pressure", "derivative", "rig_type", "target"
+
+    @property
+    def array_of_nones(self):
+        """1-D ndarray: ``NumPy`` array with ``None`` values."""
+        return np.array([None] * len(self.index))
+
+    def _reraise_exceptions(self, results):
+        """Reraise all exceptions in the ``results`` list.
+
+        Parameters
+        ----------
+        results : list
+            Post function computation results.
+
+        Raises
+        ------
+        RuntimeError
+            If any paralleled action raised an ``Exception``.
+        """
+        if ds.any_action_failed(results):
+            all_errors = self.get_errors(results)
+            raise RuntimeError("Cannot assemble the batch", all_errors)
+
+    @ds.action
+    def load(self, fmt=None, components=None, *args, **kwargs):
+        """Load given batch components.
+
+        This method supports loading of data from npz format.
+
+        Parameters
+        ----------
+        fmt : str, optional
+            Source format.
+        components : str or array-like, optional
+            Components to load.
+
+        Returns
+        -------
+        batch : RadialBatch
+            Batch with loaded components. Changes batch data inplace.
+        """
+
+        return self._load(fmt, components)
+
+    @ds.inbatch_parallel(init="indices", post="_assemble_load", target="threads")
+    def _load(self, indice, fmt=None, components=None, *args, ** kwargs):
+        """
+        Load given components from file.
+
+        Parameters
+        ----------
+        fmt : str, optional
+            Source format.
+        components : iterable, optional
+
+        Returns
+        -------
+        batch : EcgBatch
+            Batch with loaded components. Changes batch data inplace.
+
+        Raises
+        ------
+        ValueError
+            If source path is not specified and batch's ``index`` is not a
+            ``FilesIndex``.
+        """
+
+        loaders = {'npz': self._load_npz}
+
+        if isinstance(self.index, ds.FilesIndex):
+            path = self.index.get_fullpath(indice)  # pylint: disable=no-member
+        else:
+            raise ValueError("Source path is not specified")
+
+        return loaders[fmt](path, components, *args, **kwargs)
+
+    @staticmethod
+    def _load_npz(path=None, components=None, *args, **kwargs):
+        """Load given components from npz file.
+
+        Parameters
+        ----------
+        path : str
+            Path to .hea file.
+        components : iterable
+            Components to load.
+
+        Returns
+        -------
+        npz_data : list
+            List of data components.
+        """
+        _ = args, kwargs
+
+        data = dict(np.load(path))
+
+        return [data[comp] if (comp in data.keys()) else None for comp in components]
+
+
+    def _assemble_load(self, results, *args, **kwargs):
+        """Concatenate results of different workers and update ``self``.
+
+        Parameters
+        ----------
+        results : list
+            Workers' results.
+
+        Returns
+        -------
+        batch : RadialBatch
+            Assembled batch. Changes components inplace.
+        """
+
+        _ = args, kwargs
+        self._reraise_exceptions(results)
+        components = kwargs.get("components", None)
+        if components is None:
+            components = self.components
+        for comp, data in zip(components, zip(*results)):
+            data = np.array(data + (None,))[:-1]
+            if comp in ['rig_type', 'target']:
+                data = np.array(data)
+            setattr(self, comp, data)
+        return self
