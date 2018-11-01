53d60cd96aca9479d65e51b351100f51ea7f0f0b
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2018-11-02 02:46:53 +0300

    Add samples maker and samples unstacker to actions; also add filter for non-positive values of derivative.

--- radial/core/radial_batch.py
+++ radial/core/radial_batch.py
@@ -1,6 +1,7 @@
 """ Batch class for radial regime regression. """
 
 import numpy as np
+import scipy as sc
 
 from .. import dataset as ds
 
@@ -21,7 +22,6 @@ class RadialBatch(ds.Batch):
     index : DatasetIndex
         Unique identifiers of rig data in the batch.
     """
-
     def __init__(self, index, preloaded=None):
         super().__init__(index, preloaded)
         self.time = self.array_of_nones
@@ -74,6 +74,8 @@ class RadialBatch(ds.Batch):
         batch : RadialBatch
             Batch with loaded components. Changes batch data inplace.
         """
+        if isinstance(components, str):
+            components = list(components)
 
         return self._load(fmt, components)
 
@@ -99,7 +101,6 @@ class RadialBatch(ds.Batch):
             If source path is not specified and batch's ``index`` is not a
             ``FilesIndex``.
         """
-
         loaders = {'npz': self._load_npz}
 
         if isinstance(self.index, ds.FilesIndex):
@@ -129,8 +130,15 @@ class RadialBatch(ds.Batch):
 
         data = dict(np.load(path))
 
-        return [data[comp] for comp in components]
+        time = data.pop('time')
+        derivative = data.pop('derivative')
 
+        order = np.argsort(time)
+
+        data['time'] = time[order]
+        data['derivative'] = derivative[order]
+
+        return [data[comp] for comp in components]
 
     def _assemble_load(self, results, *args, **kwargs):
         """Concatenate results of different workers and update ``self``.
@@ -145,10 +153,11 @@ class RadialBatch(ds.Batch):
         batch : RadialBatch
             Assembled batch. Changes components inplace.
         """
-
         _ = args, kwargs
         self._reraise_exceptions(results)
         components = kwargs.get("components", None)
+        # import pdb
+        # pdb.set_trace()
         if components is None:
             components = self.components
         for comp, data in zip(components, zip(*results)):
@@ -157,3 +166,151 @@ class RadialBatch(ds.Batch):
                 data = np.array([d for d in data])
             setattr(self, comp, data)
         return self
+
+    @ds.action
+    @ds.inbatch_parallel(init="indices", target="threads")
+    def filter_positive(self, index):
+        """
+        Leaves only positive values.
+        """
+        i = self.get_pos(None, 'time', index)
+        time = self.time[i]
+        derivative = self.derivative[i]
+
+        mask = derivative > 0
+
+        self.time[i] = time[mask]
+        self.derivative[i] = derivative[mask]
+
+        return self
+
+    @ds.action
+    @ds.inbatch_parallel(init="indices", target="threads")
+    def get_samples(self, index, n_points, n_samples=1,
+                    sampler=None, interpolate='linear', seed=None):
+        """ Draws samples from the interpolation of time and derivative components.
+
+        Performs interpolation of the `derivative` on time using
+        `scipy.interpolate.interp1d`, samples `n_points` within `time` component
+        range, and calculates values of `derivative` in sampled points.
+        Sampler should return values from [0, 1] interval, which later is
+        stretched to the range on `time` component. Size of the sample equals
+        `(n_samples, n_points)`.
+
+        Parameters
+        ----------
+        n_points : int
+            Size of a sample.
+        n_samples : int, optional
+            Numper of samples. Defaults to `1`.
+        sampler : function or named expression
+            Method to sample points within [0, 1] range.
+            If callable, it should only take `size` as an argument.
+        interpolate : str, optional
+            Specifies the kind of interpolation as a string
+            ('linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic',
+            'previous', 'next', where 'zero', 'slinear', 'quadratic' and 'cubic'
+            refer to a spline interpolation of zeroth, first, second or third
+            order; 'previous' and 'next' simply return the previous or next value
+            of the point) or as an integer specifying the order of the spline
+            interpolator to use.
+        seed : int, optional
+            Numpy seed to be fixed. Defaults to None.
+        kwargs : misc
+            Named arguments to the sampling
+            or interpolation functions.
+
+        Returns
+        -------
+        batch : RadialBatch
+            Batch with changed `time` and `derivative`
+            components. Changes components inplace.
+
+        Note
+        ----
+        Sampler should have following parameters: `tmin`, `tmax`, `size`. Other
+        parameters to this function should be passed via kwargs.
+        See ``beta_sampler`` method for example.
+        """
+        if seed:
+            np.random.seed(seed)
+
+        i = self.get_pos(None, 'time', index)
+        time = self.time[i]
+        derivative = self.derivative[i]
+
+        interpolater = sc.interpolate.interp1d(time, derivative, kind=interpolate,
+                                               bounds_error=True, assume_sorted=True)
+
+        if  isinstance(sampler, ds.named_expr.R):
+            sampler = ds.R(sampler.name, **sampler.kwargs, size=(n_samples, n_points))
+            samples = sampler.get()
+        elif callable(sampler):
+            samples = sampler(size=(n_samples, n_points))
+        else:
+            raise ValueError("You should specify sampler function!")
+
+        tmin, tmax = np.min(time), np.max(time)
+        sample_times = samples * (tmax - tmin) + tmin
+
+        sample_derivatives = interpolater(sample_times)
+
+        self.time[i] = np.array(sample_times)
+        self.derivative[i] = np.array(sample_derivatives)
+
+        return self
+
+    @ds.action
+    def unstack_samples(self):
+        """Create a new batch in which each element of `time` and `derivative`
+        along axis 0 is considered as a separate signal.
+
+        This method creates a new batch and unstacks components `time` and
+        `derivative`. Then the method updates other components by replication if
+        they are ndarrays of objects. Other types of components will be discarded.
+
+        Returns
+        -------
+        batch : same class as self
+            Batch with split `time` and `derivative` and replicated other components.
+
+        Examples
+        --------
+        >>> batch.time
+        array([array([[ 0,  1,  2,  3],
+                      [ 4,  5,  6,  7],
+                      [ 8,  9, 10, 11]])],
+              dtype=object)
+
+        >>> batch = batch.unstack_signals()
+        >>> batch.time
+        array([array([0, 1, 2, 3]),
+               array([4, 5, 6, 7]),
+               array([ 8,  9, 10, 11])],
+              dtype=object)
+        """
+        n_reps = [sig.shape[0] for sig in self.time]
+
+        # Adding [None] to the list and removing it from resulting
+        # array to make ndarray of arrays
+        time = np.array([sample for observation in self.time
+                         for sample in observation] + [None])[:-1]
+        derivative = np.array([sample for observation in self.derivative
+                               for sample in observation] + [None])[:-1]
+
+        index = ds.DatasetIndex(len(time))
+        batch = type(self)(index)
+        batch.time = time
+        batch.derivative = derivative
+
+        for component_name in set(self.components).intersection({"rig_type", "target"}):
+            component = getattr(self, component_name)
+            val = [elem for elem, n in zip(component, n_reps) for _ in range(n)]
+            val = np.array(val + [None])[:-1]
+            setattr(batch, component_name, val)
+
+        return batch
+
+    # @ds.action
+    # @ds.inbatch_parallel(init="indices", target="threads")
+    # def log(self, src, dst)
