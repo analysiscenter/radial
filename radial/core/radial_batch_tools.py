12d8209a70cf475412d6f2b6a3d3d72875aa7cbc
Darima Mylzenova <d.mylzenova@analysiscenter.org>
2019-03-15 21:57:24 +0000

    upd batch tools

--- radial/core/radial_batch_tools.py
+++ radial/core/radial_batch_tools.py
@@ -164,3 +164,29 @@ def calculate_results(pipeline):
     sorted_ix = np.argsort(diff)
     names = np.array(names)[sorted_ix]
     return results, names
+
+def draw_results(btch, metrics, start=1):
+    """ Plot log data with target and predictions.
+
+    Parameters
+    ----------
+    btch : RadialBatch
+        predicted batch
+    metrics : np.array
+        array with metrics corresponding to every element of the batch.
+    start : int
+        first index to draw results from in the sorted batch.
+    """
+    _, ax = plt.subplots(4, 3, figsize=(20, 12))
+    ax = ax.reshape(-1)
+    ix = np.argsort(metrics)
+    for i in range(start, start+12):
+        ax[i-start].scatter(btch.log_time[ix][-i], btch.log_derivative[ix][-i], c='r')
+        ax[i-start].hlines(btch.log_target[ix][-i],
+                       np.min(btch.log_time[ix][-i]),
+                       np.max(btch.log_time[ix][-i]), colors='g',
+                       linestyle='-')
+        ax[i-start].hlines(btch.denorm_predictions[ix][-i], np.min(btch.log_time[ix][-i]), 
+                   np.max(btch.log_time[ix][-i]), colors='b', linestyle='-')
+        
+        ax[i-start].set_title('ape={0:.2f}, idx={1}'.format(metrics[ix][-i], btch.indices[ix][-i]))
